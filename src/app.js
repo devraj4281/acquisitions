4e9c2df92d580594c83e97e357f33e8bd5452649
devraj42817 <rajdev20774@gmail.com>
2026-08-22 22:29:05 +0530

    feat: initialize Express application and configure Neon database connection

--- src/app.js
+++ src/app.js
@@ -1,29 +1,52 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import logger from '#config/logger.js';
+import logger from './config/logger';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import authRoutes from './routes/auth.routes';
 
 dotenv.config();
 
 const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('combined', {
+    stream: {
+        write: (message) => logger.info(message.trim()),
+    },
+}));
+
+
 app.get('/', (req, res) => {
-  logger.info('Hello from Acquisition Project');
-  res.status(200).send('Hello From Acquistion Project');
+    logger.info('Hello from Acquisition Project');
+    res.status(200).send('Hello From Acquistion Project');
 });
 app.post('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
 app.put('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
 app.delete('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
 app.use((err, req, res, _next) => {
-  logger.error(err.message, { stack: err.stack });
-  res.status(500).send('Something broke!');
+    console.error(err);
+    res.status(500).send('Something broke!');
 });
-
+app.use('/api/auth', authRoutes);
+app.get('/api', (req, res) => {
+    res.status(200).json({ message: 'API is running' });
+})
+app.get('/health', (req, res) => {
+    res.status(200).json({ status: 'ok', timestamp: new Date().toISOString(), uptime: process.uptime() });
+});
 export default app;
--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
-import * as schema from '../db/schema.js';
+import * as schema from '../models/user.model.js';
 
 const sql = neon(process.env.DATABASE_URL);
 export const db = drizzle({ client: sql, schema });
